855cb5c9da69e7e0fa7953ab575a823d16d7bbfd
avinashnagandla16 <107924007+avinashnagandla16@users.noreply.github.com>
2024-06-14 20:00:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("fb").addEventListener("click", function() {
+  window.open("https://www.facebook.com/your-facebook-page", "_blank");
+});
+
+document.getElementById("ig").addEventListener("click", function() {
+  window.open("https://www.instagram.com/your-instagram-page", "_blank");
+});
+
+document.getElementById("tw").addEventListener("click", function() {
+  window.open("https://www.twitter.com/your-twitter-page", "_blank");
+});
+
+document.getElementById("gh").addEventListener("click", function() {
+  window.open("https://github.com/avinashnagandla16", "_blank");
+});
+
+document.getElementById("yt").addEventListener("click", function() {
+  window.open("https://www.youtube.com/channel/your-youtube-channel-id", "_blank");
+});
